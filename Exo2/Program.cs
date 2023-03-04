da9d45a392dd8ea35bb3ea0e2ba447e46c2f4713
Linden Brochu <lindenb477@hotmail.com>
2023-03-04 08:25:00 -0500

    Exo 2

---
+++ Exo2/Program.cs
@@ -0,0 +1,21 @@
+﻿using CoreData;
+
+var o = new Order();
+
+var patate = new Item
+{
+    Name = "Patate",
+    Price = 2d,
+};
+
+var carotte = new Item
+{
+    Name = "Carotte",
+    Price = 5d,
+};
+
+var pimentJalapenoSuperSpicy2000ProGamerRgb360NoScope = new Item
+{
+    Name = "Piment jalapeno super spicy 2000 pro gamer rgb 360 no scope",
+    Price = 150d,
+};
